6256b15814bcf40234d45d920d2f05f242b337f4
Duckerl <55401005+Duckerl@users.noreply.github.com>
2024-02-10 15:23:37 +0100

    Update beerScale.ino

--- beerScale.ino
+++ beerScale.ino
@@ -1,29 +1,33 @@
+// Erstellt für das Erstsemesterseminar "Einführung in die Verfahrenstechnik" der HSD, WiSe 2023
 // Demo unter https://wokwi.com/projects/383989569588126721
+// Exzellente Anleitung über die Funktionsweise von Buttons https://starthardware.org/arduino-und-taster/
+// Kallibrierungscode der offiziellen HX711 Bibliothek https://github.com/sparkfun/HX711-Load-Cell-Amplifier/tree/master/firmware/SparkFun_HX711_Calibration 
 
-#include "HX711.h"
-#include "LiquidCrystal_I2C.h"
+#include "HX711.h"                    // Bibliothek der Wägezellen
+#include "LiquidCrystal_I2C.h"        // Bibliothek 
 LiquidCrystal_I2C lcd(0x27, 20, 4);   // Standard Addresse der meisten PCF8574 Module, hier mit 4x20 Zeichen
 
-#define DOUT_PIN_1 4
+// !häufige Fehlerquelle: Pins aus dem Programm stimmen nicht mit den aus den Kommentaren überein
+#define DOUT_PIN_1 4  //Zelle 1 auf 4 & 5;
 #define SCK_PIN_1 5
 
-#define DOUT_PIN_2 6
+#define DOUT_PIN_2 6  //Zelle 2 auf 6 & 7;
 #define SCK_PIN_2 7
 
-#define DOUT_PIN_3 8
+#define DOUT_PIN_3 8  //Zelle 3 auf 8 & 9;
 #define SCK_PIN_3 9
 
-#define DOUT_PIN_4 11
-#define SCK_PIN_4 12
+#define DOUT_PIN_4 10 //Zelle 4 auf 10 & 11;
+#define SCK_PIN_4 11
 
-#define BUTTON_PIN 13 // Pin für den Button
+#define BUTTON_PIN 13  // Pin für den Button;
 
 HX711 scale1, scale2, scale3, scale4;
 
-float calibration_factor_1 = -105000; // erfolgreich -105000;
-float calibration_factor_2 = -105000; // erfolgreich -105000; 
-float calibration_factor_3 = 105000; // erfolgreich 105000; 
-float calibration_factor_4 = 105000; // erfolgreich 105000; 
+float calibration_factor_1 = -104500;  // erfolgreich -105000;
+float calibration_factor_2 = -105000;  // erfolgreich -105000;
+float calibration_factor_3 = 105000;   // erfolgreich 105000;
+float calibration_factor_4 = 105000;   // erfolgreich 105000;
 
 const int SDA_PIN = A4;  // am Arduino Mega gibts speziell zugewiesene PINs für SDA, daher eigentlich redundant
 const int SCL_PIN = A5;  // am Arduino Mega gibts speziell zugewiesene PINs für SCL, daher eigentlich redundant
@@ -46,7 +50,7 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // Initialisiert den Button, INPUT_PULLUP initiallisiert einen digitalen Resistor zum Entstören des Signals
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Initialisiert den Button, INPUT_PULLUP initiallisiert einen digitalen Resistor zum Entstören des Signals
 }
 
 void loop() {
@@ -60,9 +64,10 @@ void loop() {
   int buttonState = digitalRead(BUTTON_PIN);
 
   // Prüft Button-Press mit Entprellung --> Button kann eine Warteperiode lang nicht gedrückt werden
-  // milis() gibt die Millisekunden seit Programmstart an 
+  // milis() gibt die Millisekunden seit Programmstart an
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    //  ist der Button LOW und war vorher HIGH?
+    // Unser Button verbleibt in seiner Position, daher:
+    // Zustandsabfrage: ist der Button LOW und war vorher HIGH?
     if (buttonState == LOW && buttonPressedDown == false) {
       Serial.println("Button Pressed");
       tare();
@@ -74,23 +79,28 @@ void loop() {
       tare();
       buttonPressedDown = false;
     }
-    lastDebounceTime = millis();  // Updatet die aktuelle Buttonauslesezeit
+    lastDebounceTime = millis();  // Updatet zur aktuelle Buttonauslesezeit
   }
 
   if (scale1.is_ready() && scale2.is_ready() && scale3.is_ready() && scale4.is_ready()) {
     // Ist das letzte Warteintervall überschritten?
     if ((millis() - lastReadingTime) >= readingInterval) {
       // Liest die Gewichte aus
-      float weight1 = scale1.get_units();
-      float weight2 = scale2.get_units();
-      float weight3 = scale3.get_units();
-      float weight4 = scale4.get_units();
+      float weight1 = scale1.get_units(5); // Durchschnitt aus 5 Messungen
+      float weight2 = scale2.get_units(5);
+      float weight3 = scale3.get_units(5);
+      float weight4 = scale4.get_units(5);
 
       // Kalkuliert das totale Gewicht
       float totalWeight = weight1 + weight2 + weight3 + weight4;
 
+      // Setze den Wert auf 0, wenn er sehr nah an 0 liegt
+      if (abs(totalWeight) < 0.001) {
+        totalWeight = 0.0; 
+        }
+
       Serial.print("Weight 1: ");
-      Serial.print(weight1, 2);
+      Serial.print(weight1, 2); // 2 Dezimalstellen anzeigen
       Serial.println(" kg");
 
       Serial.print("Weight 2: ");
@@ -113,17 +123,16 @@ void loop() {
       lcd.setCursor(0, 0);
       lcd.print("Totales Gewicht: ");
       lcd.setCursor(0, 1);
-      lcd.print(totalWeight, 2);
+      lcd.print(totalWeight, 2); 
       lcd.print(" kg");
 
-    lastReadingTime = millis(); // Updatet die aktuelle Waageauslesungszeit 
+      lastReadingTime = millis();  // Updatet zur aktuellen Waageauslesungszeit
     }
-  }    
+  }
 }
 
 // Funktion fürs  Nullsetzen, .tare wird 2x ausgeführt für ein genaueres Ergebnis
 void tare() {
-  //lcd.setCursor(0, 3);
   Serial.println("updating.");
   lcd.clear();
   lcd.print("===tare===");
@@ -134,8 +143,7 @@ void tare() {
   lcd.setCursor(0, 1);
   lcd.print("updating.");
 
-  delay(100); // kleiner Delay
-  //lcd.clear();
+  delay(100);  // kleiner Delay
   Serial.println("updating..");
   lcd.setCursor(0, 2);
   lcd.print("updating..");
@@ -143,7 +151,6 @@ void tare() {
   scale2.tare();
   scale3.tare();
   scale4.tare();
-  //lcd.clear();
   Serial.println("updating...");
   lcd.setCursor(0, 3);
   lcd.print("updating...");
