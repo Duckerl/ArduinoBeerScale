ba751b11a75c1c632180aea0c9b16ddccc3f7f02
Duckerl <55401005+Duckerl@users.noreply.github.com>
2024-02-10 12:30:15 +0100

    Update beerScale.ino

--- beerScale.ino
+++ beerScale.ino
@@ -1,4 +1,4 @@
-// Demo at https://wokwi.com/projects/383989569588126721
+// Demo unter https://wokwi.com/projects/383989569588126721
 
 #include "HX711.h"
 #include "LiquidCrystal_I2C.h"
@@ -16,7 +16,7 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);   // Standard Addresse der meisten PCF8574 M
 #define DOUT_PIN_4 11
 #define SCK_PIN_4 12
 
-#define BUTTON_PIN 13 // Pin for the push button
+#define BUTTON_PIN 13 // Pin für den Button
 
 HX711 scale1, scale2, scale3, scale4;
 
@@ -25,8 +25,10 @@ float calibration_factor_2 = -105000; // erfolgreich -105000;
 float calibration_factor_3 = 105000; // erfolgreich 105000; 
 float calibration_factor_4 = 105000; // erfolgreich 105000; 
 
-const int SDA_PIN = A4;  // am Arduino Mega gibts speziell zugewiesene PINs für SDA
-const int SCL_PIN = A5;  // am Arduino Mega gibts speziell zugewiesene PINs für SCL
+const int SDA_PIN = A4;  // am Arduino Mega gibts speziell zugewiesene PINs für SDA, daher eigentlich redundant
+const int SCL_PIN = A5;  // am Arduino Mega gibts speziell zugewiesene PINs für SCL, daher eigentlich redundant
+
+bool buttonPressedDown = true;  //speichert den letzten Zustand des Buttons; theoretisch weiß der Button bei der Initailisierung nicht ob der Zustand gedrückt (=true) ist oder nicht, ist praktisch aber egal
 
 void setup() {
   Serial.begin(9600);
@@ -48,36 +50,43 @@ void setup() {
 }
 
 void loop() {
-  // Check if the tare button is pressed
 
-  static unsigned long lastDebounceTime = 0;
-  static unsigned long debounceDelay = 200;
-  static unsigned long lastReadingTime = 0;
-  static unsigned long readingInterval = 1000; // Interval between readings in milliseconds
+  static unsigned long lastDebounceTime = 0;    // speichert den Zeitpunkt des letzten Buttonpress
+  static unsigned long debounceDelay = 1000;    // Warteintervall für den Button in Milisec; 1000 milisec = 1 sec
+  static unsigned long lastReadingTime = 0;     // speichert den Zeitpunkt der letzten Waagenlesung
+  static unsigned long readingInterval = 1000;  // Warteintervall zur nächsten Waagenlesung in Millisec
 
-  // Read the state of the button
+  // Variable zum Auslesen des Button-Status (HIGH oder LOW)
   int buttonState = digitalRead(BUTTON_PIN);
 
-  // Check for button press with debouncing
-  // milis() gibt die Millisekunden seit rogrammstart an 
+  // Prüft Button-Press mit Entprellung --> Button kann eine Warteperiode lang nicht gedrückt werden
+  // milis() gibt die Millisekunden seit Programmstart an 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (buttonState == LOW) {
+    //  ist der Button LOW und war vorher HIGH?
+    if (buttonState == LOW && buttonPressedDown == false) {
       Serial.println("Button Pressed");
       tare();
+      buttonPressedDown = true;
     }
-    lastDebounceTime = millis();
+    // ist der Button HIGH und war vorher LOW?
+    if (buttonState == HIGH && buttonPressedDown == true) {
+      Serial.println("Button Pressed");
+      tare();
+      buttonPressedDown = false;
+    }
+    lastDebounceTime = millis();  // Updatet die aktuelle Buttonauslesezeit
   }
 
   if (scale1.is_ready() && scale2.is_ready() && scale3.is_ready() && scale4.is_ready()) {
-    // Check if it's time for a new reading
+    // Ist das letzte Warteintervall überschritten?
     if ((millis() - lastReadingTime) >= readingInterval) {
-      // Read weights from each load cell
+      // Liest die Gewichte aus
       float weight1 = scale1.get_units();
       float weight2 = scale2.get_units();
       float weight3 = scale3.get_units();
       float weight4 = scale4.get_units();
 
-      // Calculate the total weight
+      // Kalkuliert das totale Gewicht
       float totalWeight = weight1 + weight2 + weight3 + weight4;
 
       Serial.print("Weight 1: ");
@@ -105,33 +114,37 @@ void loop() {
       lcd.print("Totales Gewicht: ");
       lcd.setCursor(0, 1);
       lcd.print(totalWeight, 2);
-      lcd.println(" kg");
+      lcd.print(" kg");
 
-    lastReadingTime = millis(); // Update the last reading time
+    lastReadingTime = millis(); // Updatet die aktuelle Waageauslesungszeit 
     }
   }    
 }
 
-// Funktion fürs  Nullsetzen
+// Funktion fürs  Nullsetzen, .tare wird 2x ausgeführt für ein genaueres Ergebnis
 void tare() {
-  lcd.setCursor(0, 3);
+  //lcd.setCursor(0, 3);
   Serial.println("updating.");
   lcd.clear();
-  lcd.println("tare");
+  lcd.print("===tare===");
   scale1.tare();
   scale2.tare();
   scale3.tare();
   scale4.tare();
+  lcd.setCursor(0, 1);
+  lcd.print("updating.");
 
-  delay(100); // Add a small delay before re-taring
-  lcd.clear();
+  delay(100); // kleiner Delay
+  //lcd.clear();
   Serial.println("updating..");
-  lcd.println("updating.");
+  lcd.setCursor(0, 2);
+  lcd.print("updating..");
   scale1.tare();
   scale2.tare();
   scale3.tare();
   scale4.tare();
-  lcd.clear();
+  //lcd.clear();
   Serial.println("updating...");
-  lcd.println("updating..");
+  lcd.setCursor(0, 3);
+  lcd.print("updating...");
 }
